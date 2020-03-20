aae0392039c7dc31596bd62dcf2d30dda236b32d
Nicolas <3316257-nicolaslps@users.noreply.gitlab.com>
2020-03-20 19:42:29 +0100

    mise à jour de l'application avec l'utilisation de l'enum ResolutionFenetre

--- src/main/java/com/montaury/citadels/ViewManager.java
+++ src/main/java/com/montaury/citadels/ViewManager.java
@@ -1,5 +1,6 @@
 package com.montaury.citadels;
 
+import com.montaury.citadels.interfaceAssets.ResolutionFenetre;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -18,7 +19,7 @@ public class ViewManager {
 
     private void loadAccueil(){
         mainPane = new Pane();
-        mainScene = new Scene(mainPane, 500, 500);
+        mainScene = new Scene(mainPane, ResolutionFenetre.accueil.getWidth(), ResolutionFenetre.accueil.getHeight());
         mainStage = new Stage();
 
         mainStage.setResizable(false);
