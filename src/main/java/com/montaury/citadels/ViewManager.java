316ebf2fc1e12d646602494ab50fc314744fbd5b
Nicolas <3316257-nicolaslps@users.noreply.gitlab.com>
2020-03-20 19:50:28 +0100

    Mise à jours de la fonction loadAccueil() et ajout du fichier main.css

--- src/main/java/com/montaury/citadels/ViewManager.java
+++ src/main/java/com/montaury/citadels/ViewManager.java
@@ -1,7 +1,11 @@
 package com.montaury.citadels;
 
+import com.montaury.citadels.interfaceAssets.BoiteAOutils;
 import com.montaury.citadels.interfaceAssets.ResolutionFenetre;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -10,7 +14,7 @@ public class ViewManager {
     private  Pane mainPane;
     private Scene mainScene;
     private Stage mainStage;
-
+    private BoiteAOutils outils = new BoiteAOutils();
 
     public ViewManager(){
 
@@ -20,7 +24,32 @@ public class ViewManager {
     private void loadAccueil(){
         mainPane = new Pane();
         mainScene = new Scene(mainPane, ResolutionFenetre.accueil.getWidth(), ResolutionFenetre.accueil.getHeight());
+        mainScene.getStylesheets().add("main.css");
+
+
+        outils.createLabel("Citadelles",28, 280, mainPane, "title");
+
+        Button buttonNouvellePartie = outils.newButton("Nouvelle partie",206, 305);
+        buttonNouvellePartie.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+
+        mainPane.getChildren().add(buttonNouvellePartie);
+
+        Button buttonQuitter = outils.newButton("Quitter",270, 305);
+        buttonQuitter.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                mainStage.close();
+            }
+        });
+
+        mainPane.getChildren().add(buttonQuitter);
         mainStage = new Stage();
+
 
         mainStage.setResizable(false);
         mainStage.setScene(mainScene);
